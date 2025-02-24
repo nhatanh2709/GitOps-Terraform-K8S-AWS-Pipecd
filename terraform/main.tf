359de7c4782f459ba3f77bdf5e90a5e0ca5e5e40
nhatanh2709 <22520059@gm.uit.edu.vn>
2025-02-24 17:20:30 +0700

    success

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
-    required_version = "~> 1.10.1"
+    required_version = ">=1.10.1"
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = ">=4.0.0"
+      }
+    }
     backend "s3" {
         bucket         = "s3-pipecd-s3-backend"
         key            = "pipecd"
